6c27645ab1821f69a11f6635a3e03085040b9248
corina278 <corina_dogaru@yahoo.com>
2023-01-15 23:03:36 +0200

    1st commit

---
+++ nav-bar.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<title>PaperTown</title>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <meta name="format-detection" content="telephone=no">
+	    <meta name="apple-mobile-web-app-capable" content="yes">
+	    <meta name="author" content="">
+	    <meta name="keywords" content="">
+	    <meta name="description" content="">
+
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+
+<div id="header-wrap">
+	
+	<div class="top-content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="right-element">
+                        <!--trebuie adaugat fisierul de sign-up-->
+						<a href="#" class="user-account for-buy"><i class="icon icon-user"></i><span>Account</span></a>
+                        <!--trebuie adaugat fisierul de cart-->
+						<a href="#" class="cart for-buy"><i class="icon icon-clipboard"></i><span>Cart:(0 $)</span></a>
+
+					</div><!--top-right / facut--> 
+				</div>
+				
+			</div>
+		</div>
+	</div><!--top-content-->
+
+	<header id="header">
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-2">
+					<div class="main-logo">
+						<a href="index.html"><img src="img/PaperTown.png" alt="logo" width="70px" height="70 px"></a>
+					</div>
+
+				</div>
+
+				<div class="col-md-10">
+					
+					<nav id="navbar">
+						<div class="main-menu stellarnav">
+							<ul class="menu-list">
+								<li class="menu-item active"><a href="#home" data-effect="Home">Home</a></li>
+								<li class="menu-item"><a href="#about" class="nav-link" data-effect="About">About</a></li>
+								<li class="menu-item has-sub">
+									<a href="#pages" class="nav-link" data-effect="Pages">Pages</a>
+
+									<ul>
+								        <li class="active"><a href="index.html">Home</a></li>
+								        <li><a href="about.html">About</a></li>
+								        <li><a href="styles.html">Styles</a></li>
+								        <li><a href="blog.html">Blog</a></li>
+								        <li><a href="single-post.html">Post Single</a></li>
+								        <li><a href="shop.html">Our Store</a></li>
+								        <li><a href="single-product.html">Product Single</a></li>
+								        <li><a href="contact.html">Contact</a></li>
+								        <li><a href="thank-you.html">Thank You</a></li>
+								     </ul>
+
+								</li>
+								<li class="menu-item"><a href="#popular-books" class="nav-link" data-effect="Shop">Shop</a></li>
+								<li class="menu-item"><a href="#latest-blog" class="nav-link" data-effect="Articles">Articles</a></li>
+								<li class="menu-item"><a href="#contact" class="nav-link" data-effect="Contact">Contact</a></li>
+							</ul>
+
+							<div class="hamburger">
+				                <span class="bar"></span>
+				                <span class="bar"></span>
+				                <span class="bar"></span>
+				            </div>
+
+						</div>
+					</nav>
+
+				</div>
+
+			</div>
+		</div>
+	</header>
+		
+</div>
+
+</body>
