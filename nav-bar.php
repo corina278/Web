5167deaa4aab245b9de81bfd4ffb7abd3e2e75d0
corina278 <corina_dogaru@yahoo.com>
2023-01-15 23:28:42 +0200

    1st new commit

--- nav-bar.php
+++ nav-bar.php
@@ -12,6 +12,7 @@
 	    <meta name="description" content="">
 
         <link rel="stylesheet" type="text/css" href="style.css">
+
     </head>
     <body>
 
@@ -52,33 +53,21 @@
 							<ul class="menu-list">
 								<li class="menu-item active"><a href="#home" data-effect="Home">Home</a></li>
 								<li class="menu-item"><a href="#about" class="nav-link" data-effect="About">About</a></li>
-								<li class="menu-item has-sub">
-									<a href="#pages" class="nav-link" data-effect="Pages">Pages</a>
+								<!-- <li class="menu-item has-sub">
+									<a href="#pages" class="nav-link" data-effect="Pages">Products</a>
 
 									<ul>
-								        <li class="active"><a href="index.html">Home</a></li>
-								        <li><a href="about.html">About</a></li>
-								        <li><a href="styles.html">Styles</a></li>
-								        <li><a href="blog.html">Blog</a></li>
-								        <li><a href="single-post.html">Post Single</a></li>
-								        <li><a href="shop.html">Our Store</a></li>
-								        <li><a href="single-product.html">Product Single</a></li>
-								        <li><a href="contact.html">Contact</a></li>
-								        <li><a href="thank-you.html">Thank You</a></li>
+								        <li><a href="styles.html">Books</a></li>
+								        <li><a href="blog.html">Albums</a></li>
+								        <li><a href="single-post.html">Movies</a></li>
 								     </ul>
 
-								</li>
-								<li class="menu-item"><a href="#popular-books" class="nav-link" data-effect="Shop">Shop</a></li>
-								<li class="menu-item"><a href="#latest-blog" class="nav-link" data-effect="Articles">Articles</a></li>
+								</li> -->
+                                <li class="menu-item"><a href="books.php" class="nav-link" >Books</a></li>
+                                <li class="menu-item"><a href="movies.php" class="nav-link" >Movies</a></li>
+                                <li class="menu-item"><a href="albums.php" class="nav-link" >Albums</a></li>
 								<li class="menu-item"><a href="#contact" class="nav-link" data-effect="Contact">Contact</a></li>
 							</ul>
-
-							<div class="hamburger">
-				                <span class="bar"></span>
-				                <span class="bar"></span>
-				                <span class="bar"></span>
-				            </div>
-
 						</div>
 					</nav>
 
