af0de53dfa70e9ea910bcf376f78d46d44d8e418
corina278 <corina_dogaru@yahoo.com>
2023-01-18 18:59:11 +0200

    3rd commit

--- albums.php
+++ albums.php
@@ -1,5 +1,20 @@
 <?php
 include("nav-bar.php");
+
+$sql = "SELECT Price FROM jewel WHERE jewel_id=1001";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> pret:". $row["Price"] ;
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
 ?>
 
 <table>
